b37b5c74f079fea6d17d727c6c31bd79cb0b9f11
Olijujuan Green <olijujuangreen@gmail.com>
2022-04-17 23:07:30 -0400

    get articles from api

--- Coinradars/NewsCell.swift
+++ Coinradars/NewsCell.swift
@@ -9,7 +9,24 @@ import UIKit
 
 class NewsCell: UICollectionViewCell {
     
-    static let newsCellId = "newsCellId"
+    static let newsCellId = "newsCellId."
+    
+    let articleImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemBlue
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let articleTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Some Really long article name going here. Some Really long article name going here"
+        label.numberOfLines = 0
+        label.sizeToFit()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,6 +39,27 @@ class NewsCell: UICollectionViewCell {
     
     func configure() {
         backgroundColor = .systemRed
+        
+        [articleImageView, articleTitleLabel].forEach { addSubview($0) }
+        
+        NSLayoutConstraint.activate([
+        
+            articleImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            articleImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+            articleImageView.topAnchor.constraint(equalTo: topAnchor),
+            articleImageView.heightAnchor.constraint(equalToConstant: 200),
+            
+            articleTitleLabel.topAnchor.constraint(equalTo: articleImageView.bottomAnchor, constant: 4),
+            articleTitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            articleTitleLabel.heightAnchor.constraint(equalToConstant: 52),
+            articleTitleLabel.widthAnchor.constraint(equalTo: widthAnchor, constant: -16)
+            
+        ])
+        
+    }
+    
+    func set(article: Article) {
+        
     }
     
 }
